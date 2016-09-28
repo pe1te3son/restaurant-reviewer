3e966b14895a23036f3341c2fe1e00d43692a74d
Peter Janak <2010janak2010@gmail.com>
2016-09-28 04:16:44 +0000

    chore: Set sass compiler

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,15 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: 'sass',
+      nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,7 +24,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
   app.import('bower_components/materialize/dist/js/materialize.js');
   return app.toTree();
 };
