59943a85e4e5b5674bd8f299a92e36b30c56319c
Peter Janak <2010janak2010@gmail.com>
2016-09-28 04:15:37 +0000

    chore: Install materializecss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/materialize/dist/js/materialize.js');
   return app.toTree();
 };
