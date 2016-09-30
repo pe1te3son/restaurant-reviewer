7a8d8a1d3b2bde748018b430e0950b15ab8d6d63
Peter Janak <2010janak2010@gmail.com>
2016-09-30 04:29:34 +0000

    feat: Fetch restaurants near me by default

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,14 +2,19 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Route.extend({
+  clientId: 'client_id=QII04JY4W2DNZNZPK4NXK4R1N3HUE4SD2OWT1FAPZIOAZJMY',
+  clientSecret: 'client_secret=CBVRYTV2JXJXQJWFD51RY0UJ51SAMAFEPZLJZWGMRD3LQKF4',
   model(){
-    const clientId = 'client_id=QII04JY4W2DNZNZPK4NXK4R1N3HUE4SD2OWT1FAPZIOAZJMY';
-    const clientSecret = 'client_secret=CBVRYTV2JXJXQJWFD51RY0UJ51SAMAFEPZLJZWGMRD3LQKF4';
-    const link = `https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d121941735&ll=51.5074,0.1278&radius=10000&${clientId}&${clientSecret}&v=20160929`;
-    return $.get(link)
-      .then((resp)=>{
-        console.log(resp);
-        return resp;
-      }).catch(()=>{ return '';});
+
+    const link = `https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&ll=51.5074,0.1278&radius=10000&${this.get('clientId')}&${this.get('clientSecret')}&v=20160929`;
+
+    return new Promise((resolve)=>{
+      return $.get(link)
+        .then((resp)=>{
+          console.log(resp);
+          return resolve(resp);
+        }).catch(()=>{ return '';});
+    });
   }
+
 });
