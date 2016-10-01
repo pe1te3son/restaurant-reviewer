4e27fbc90832a8e509a1d5c7676c7b057862d798
Peter Janak <2010janak2010@gmail.com>
2016-10-01 04:35:22 +0000

    feat: Add restaurant price category helper.

---
+++ app/helpers/price-category.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export function priceCategory(params/*, hash*/) {
+
+  if(!params[0]) { return ''; }
+
+  const venue = params[0];
+
+  if(venue.price){
+    let priceTag = venue.price.currency.repeat(venue.price.tier);
+    return priceTag;
+  }
+
+  const priceSummary = venue.attributes.groups.findBy('name', 'Price').summary;
+  return priceSummary ? priceSummary : '';
+
+}
+
+export default Ember.Helper.helper(priceCategory);
---
+++ tests/unit/helpers/price-category-test.js
@@ -0,0 +1,10 @@
+import { priceCategory } from 'restaurant-reviewer/helpers/price-category';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | price category');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = priceCategory([42]);
+  assert.ok(result);
+});
