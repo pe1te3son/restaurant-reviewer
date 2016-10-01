d9eb5a57a911510ed36b6f8dbf73293d6f71e48b
Peter Janak <2010janak2010@gmail.com>
2016-10-01 03:44:16 +0000

    feat: Return rating for each restaurant

---
+++ app/helpers/star-rating.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export function starRating(params/*, hash*/) {
+  const [rating] = params;
+
+  let ratingEl = '';
+  let currentRating = Math.round(rating/2);
+  for(var i=0; i< 5; i++){
+    if(i >= currentRating){
+      ratingEl += '<i class="material-icons star-rate">star_rate</i>';
+      continue;
+    }
+    ratingEl += '<i class="material-icons star-rate highlight">star_rate</i>';
+  }
+  return Ember.String.htmlSafe(ratingEl);
+}
+
+export default Ember.Helper.helper(starRating);
---
+++ tests/unit/helpers/star-rating-test.js
@@ -0,0 +1,10 @@
+import { starRating } from 'restaurant-reviewer/helpers/star-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | star rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = starRating([42]);
+  assert.ok(result);
+});
