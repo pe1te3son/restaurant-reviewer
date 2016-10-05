598ec515a93ce345e5b72f818979ffd3156a6fc5
Peter Janak <2010janak2010@gmail.com>
2016-10-05 22:34:54 +0000

    refactor: Use fetch instead of get

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,20 +1,28 @@
 import Ember from 'ember';
-import $ from 'jquery';
+//import $ from 'jquery';
 
 export default Ember.Route.extend({
   clientId: 'client_id=QII04JY4W2DNZNZPK4NXK4R1N3HUE4SD2OWT1FAPZIOAZJMY',
   clientSecret: 'client_secret=CBVRYTV2JXJXQJWFD51RY0UJ51SAMAFEPZLJZWGMRD3LQKF4',
   model(){
-
+    //https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&ll=51.5074,0.1278&radius=10000&
     const link = `https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&ll=51.5074,0.1278&radius=10000&${this.get('clientId')}&${this.get('clientSecret')}&v=20160929`;
 
-    return new Promise((resolve)=>{
-      return $.get(link)
-        .then((resp)=>{
-          console.log(resp);
-          return resolve(resp);
-        }).catch(()=>{ return '';});
+    return fetch(link).then((response)=>{
+      return response.json();
+    })
+    .then((respJson)=>{
+      console.log(respJson);
+      return respJson;
     });
+
+    // return new Promise((resolve)=>{
+    //   return $.get(link)
+    //     .then((resp)=>{
+    //       console.log(resp);
+    //       return resolve(resp);
+    //     }).catch(()=>{ return '';});
+    // });
   }
 
 });
