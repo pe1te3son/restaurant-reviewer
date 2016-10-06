60246739db894c07728f28edb3d8ded063f57f26
Peter Janak <2010janak2010@gmail.com>
2016-10-06 04:01:14 +0000

    feat: Set form validation

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Controller.extend({
   autocompleteSrv: Ember.inject.service('geolocate'),
+  showWarning: false,
+  searchInput: null,
+
+  init(){
+    Ember.run.schedule('afterRender', ()=>{
+      $('#current-location').focus();
+    });
+  },
 
   actions: {
     autocompleteFn(){
@@ -10,14 +19,10 @@ export default Ember.Controller.extend({
 
     submitSearch(event){
       event.preventDefault();
-      const place = this.get('autocompleteSrv').getPlace();
-      if(place){
-        // this.transitionToRoute('search-results', {
-        //   queryParams: {
-        //     lat: `${place.geometry.location.lat()}_${ place.geometry.location.lng()}`
-        //   }
-        // });
+      const place = this.get('autocompleteSrv').getCurrentPlace();
 
+      if(place && this.get('searchInput').length > 0){
+        this.set('showWarning', false);
         this.transitionToRoute('search-results', {
           queryParams: {
             lat: place.geometry.location.lat(),
@@ -25,8 +30,13 @@ export default Ember.Controller.extend({
           }
         });
       } else {
-        console.log('wrong');
+        this.set('showWarning', true);
+        $('#current-location').focus();
       }
+    },
+
+    preventFormFromSubmiting(event){
+      event.preventDefault();
     }
   },//actions
 
