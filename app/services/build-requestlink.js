a06a1211a8ba336d54813fb7d422da4dae091328
Peter Janak <2010janak2010@gmail.com>
2016-10-10 22:55:15 +0100

    feat: Builds foursquare request url

---
+++ app/services/build-requestlink.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  //https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&ll=51.5074,0.1278&radius=10000&
+  clientId: 'client_id=QII04JY4W2DNZNZPK4NXK4R1N3HUE4SD2OWT1FAPZIOAZJMY',
+  clientSecret: 'client_secret=CBVRYTV2JXJXQJWFD51RY0UJ51SAMAFEPZLJZWGMRD3LQKF4',
+
+  build(lat, lng){
+    return `https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&ll=${lat},${lng}&radius=10000&${this.get('clientId')}&${this.get('clientSecret')}&v=20160929`;
+  }
+});
