8c478517594e5b53e558661bf8bc6a85112205bd
Peter Janak <2010janak2010@gmail.com>
2016-10-11 00:46:26 +0100

    feat: Fetch restaurant

--- app/routes/restaurant.js
+++ app/routes/restaurant.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  requestLink: Ember.inject.service('build-requestlink'),
+
+  model(params){
+    const link = this.get('requestLink').idLink(params.id);
+    return fetch(link).then((response)=>{
+      return response.json();
+    }).then((responseJson)=>{
+      return responseJson.response.venue;
+    });
+  }
+
 });
