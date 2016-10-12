7b8f7f722b37da032c3aec6f6de329ccad194399
Peter Janak <2010janak2010@gmail.com>
2016-10-12 03:10:49 +0100

    feat: Update mdl componentHandler when new restaurant is loaded

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -2,8 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   init(){
+
+  },
+
+  modelHasChange: function(){
+
     Ember.run.schedule('afterRender', ()=>{
+      /* global componentHandler */
       console.log(this.get('model'));
+      componentHandler.upgradeAllRegistered();
     });
-  }
+
+
+  }.observes('model'),
 });
