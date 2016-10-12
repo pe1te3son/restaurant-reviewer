d64d02716433ca4ddc28ecb4fc97fbb8cb4f439e
Peter Janak <2010janak2010@gmail.com>
2016-10-12 08:35:14 +0100

    feat: Add slice array helper
    Returns array sliced from index 0 to chosen index

---
+++ tests/unit/helpers/slice-array-test.js
@@ -0,0 +1,10 @@
+import { sliceArray } from 'restaurant-reviewer/helpers/slice-array';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | slice array');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = sliceArray([42]);
+  assert.ok(result);
+});
