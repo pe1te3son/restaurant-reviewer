1aa6c13be07e0ec10e8a3d895dfb6419b5a270d9
Peter Janak <2010janak2010@gmail.com>
2016-10-13 03:51:20 +0100

    feat: Set go back from restaurant page button

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -1,18 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  init(){
-
-  },
 
   modelHasChange: function(){
 
     Ember.run.schedule('afterRender', ()=>{
       /* global componentHandler */
-      console.log(this.get('model'));
       componentHandler.upgradeAllRegistered();
     });
 
-
   }.observes('model'),
+
+  actions: {
+    goBack(){
+      history.back();
+    }
+  }
 });
