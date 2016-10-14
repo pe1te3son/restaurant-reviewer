a6ab2a63ea41a185f0cd5c25932f4ff13a65282c
Peter Janak <2010janak2010@gmail.com>
2016-10-14 02:20:01 +0100

    feat: Add circle loader component

--- app/components/restaurant-comments.js
+++ app/components/restaurant-comments.js
@@ -1,4 +1,41 @@
 import Ember from 'ember';
+import $ from 'jquery';
+import { sliceArray } from 'restaurant-reviewer/helpers/slice-array';
 
+/**
+* @name Restaurant Comments / Reviews
+* @desc Restaurant Comments Component
+*/
 export default Ember.Component.extend({
+  currentComments: null,
+
+  didReceiveAttrs(){
+    // Show first five only
+    this.set('currentComments', sliceArray([
+      this.get('comments'),
+      5
+    ]));
+  },
+
+  actions: {
+    /**
+    * @name Load More Comments
+    * @desc Loads Comments on click
+    */
+    loadMoreComments(){
+      const currentlyOnScreen = $('#all-comments').children().length - 1;
+      // Show loader
+      this.sendAction('initLoader', true);
+
+      this.set('currentComments', sliceArray([
+        this.get('comments'),
+        currentlyOnScreen + 15
+      ]));
+
+      Ember.run.later(()=>{
+        // Hide loader
+        this.sendAction('initLoader', false);
+      }, 500);
+    }
+  }
 });
