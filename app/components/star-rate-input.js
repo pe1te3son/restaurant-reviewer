48038636ba7e9af8e8650e10c8e7f91815b10817
Peter Janak <2010janak2010@gmail.com>
2016-10-15 04:43:19 +0100

    feat: Highlight star rating element when radio buttons are in focus

--- app/components/star-rate-input.js
+++ app/components/star-rate-input.js
@@ -7,6 +7,14 @@ import $ from 'jquery';
 */
 export default Ember.Component.extend({
   didInsertElement(){
+      $("input[name=rating]:radio").on('focus', function(){
+        $('#star-rating-cont').css({'border-bottom': '2px solid #007dba'});
+      });
+
+      $("input[name=rating]:radio").on('blur', function(){
+        $('#star-rating-cont').css({'border-bottom': 'none'});
+      });
+
       $("input[name=rating]:radio").bind( "change", function( /*event*/ ) {
       console.log('Lang: '+$(this).val());
       // Call function here
