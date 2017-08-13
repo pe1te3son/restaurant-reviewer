2492460122bccd059f1b7f5696432f645cdc3933
Peter Janak <2010janak2010@gmail.com>
2017-08-13 14:01:35 +0100

    Added google map component

---
+++ app/components/google-map.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import $ from 'jquery';
+
+export default Ember.Component.extend({
+  didReceiveAttrs(){
+    this._super(...arguments);
+  },
+
+  didInsertElement(){
+    const $mapEl = $('#google-map');
+    const lat = this.get('lat');
+    const lng = this.get('lng');
+    const map = new google.maps.Map($mapEl[0], {
+      zoom: 14,
+      center: {
+        lat,
+        lng
+      },
+      disableDefaultUI: true
+    });
+
+
+    const listener = map.addListener('tilesloaded', () => {
+      // $mapEl.find('a').attr('tabindex', 999);
+      // google.maps.event.removeListener(listener)
+      // const test = $('[tabindex]').parent('.gm-style');
+      const test = $mapEl.find('div[tabindex="0"]').attr('tabindex', "-1");
+    });
+  }
+});
